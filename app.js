8e69b9b73eb1db7075fba69e961ced81534ebab6
sambuddha7 <sbiswas7@illinois.edu>
2023-07-10 02:21:58 +0530

    gauth

--- app.js
+++ app.js
@@ -101,7 +101,7 @@ passport.deserializeUser(function (user, cb) {
 passport.use(new GoogleStrategy({
   clientID: process.env.CLIENT_ID,
   clientSecret: process.env.CLIENT_SECRET,
-  callbackURL: "http://localhost:3000/auth/google/signup-2"
+  callbackURL: "https://saporai.com/auth/google/signup-2"
   // userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
 },
 function (accessToken, refreshToken, profile, cb) {
