2c4f5704fbc1ab18c7d1658e83de29f6b5dfc62d
sambuddha7 <sbiswas7@illinois.edu>
2024-01-02 22:47:28 +0530

    refactored db, youtube fn, user dashboard route

--- app.js
+++ app.js
@@ -17,9 +17,13 @@ const axios = require('axios');
 //api 
 const {Configuration, OpenAIApi} = require("openai");
 
-//api key auth
+//diff file 
+
+const {fetchYouTubeData} = require("./views/api-calls/youtubeApi.js");
+
+//open ai api key auth
 const config = new Configuration({
-  apiKey: process.env.API_KEY
+  apiKey: process.env.API_KEY // open ai
 });
 //instance of openai
 const openai = new OpenAIApi(config);
@@ -46,52 +50,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.json());
 
 
-const mongopw = process.env.MONGOPW;
-const youtubeKey = process.env.YOUTUBE_KEY;
-mongoose.connect(`mongodb+srv://sbiswas7:${mongopw}@cluster0.mtk5ama.mongodb.net/userDB`, {useNewUrlParser: true});
-// mongoose.connect("mongodb://localhost:27017/usertestDB", {useNewUrlParser: true});
-
-const db = mongoose.connection;
-
-
-
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-  console.log("MongoDB database connection established successfully");
-});
-
-const userSchema = new mongoose.Schema({
-  // email: {type: String, unique: true, sparse: true},
-  password: String,
-  name: String,
-  age: Number,
-  height: Number,
-  weight: Number,
-  allergy: [String],
-  activity: String,
-  gender: String,
-  googleId: String,
-  preference: String,
-  goal: String,
-  maintenance: Number,
-  recipeHistory: {
-    type: [[String]],
-    default: []
-  },
-  favorites: {
-    type: [[String]],
-    default: []
-  },
-  weightUnit: {
-    type: String,
-    default: "lbs"
-  }
-});
-userSchema.plugin(passportLocalMongoose);
-userSchema.plugin(findOrCreate);
-
-
-const User = mongoose.model('User', userSchema);
+const { User } = require('./views/api-calls/db.js');
 
 passport.use(User.createStrategy());
 
@@ -220,60 +179,11 @@ app.get("/tr1", function(req, res) {
     res.render("../login");
   }
 });
-app.get("/user", function(req, res) {
+const getUserRoute = require('./views/routes/getUser.js');
 
-  if (req.isAuthenticated()) {
-    User.findById(req.user.id).then((foundUser) => {
-      if (foundUser) {
-        var weight = foundUser.weight;
-        const actualWeight = weight;
-        const weightUnit = foundUser.weightUnit;
-        if (weightUnit == "kgs") {
-          weight = weight * 2.20462;
-          weight = Math.round(weight);
-        }
-        const height = foundUser.height;
-        const age = foundUser.age;
-        const gender = foundUser.gender;
-        const activityLevel = foundUser.activity;
-        if (gender === 'Male' || gender === 'Other') {
-          // For males: BMR = 66 + (6.23 × weight in lbs) + (12.7 × height in inches) - (6.8 × age in years)
-          bmr = Math.floor(66 + (6.23 * weight) + (12.7 * height) - (6.8 * age));
-        } else if (gender === 'Female') {
-          // For females: BMR = 655 + (4.35 × weight in lbs) + (4.7 × height in inches) - (4.7 × age in years)
-          bmr = Math.floor(655 + (4.35 * weight) + (4.7 * height) - (4.7 * age));
-        }
-        const activityLevels = {
-          Sedentary: 1.2, // Little to no exercise
-          Light: 1.375,   // Light exercise/sports 1-3 days per week
-          Moderate: 1.55, // Moderate exercise/sports 3-5 days per week
-          Active: 1.725,  // Active exercise/sports 6-7 days per week
-          VeryActive: 1.9 // Very active exercise/sports & physical job or 2x training
-        };
 
-        var maintenanceCalories = Math.round(bmr * activityLevels[activityLevel]);
-        if (foundUser.goal == "WeightLoss") {
-          maintenanceCalories -= 250;
-        } else if (foundUser.goal == "WeightGain") {
-          maintenanceCalories += 100;
-        }
-        foundUser.maintenance = maintenanceCalories;
-        foundUser.save().then(()=> {
-          res.render("user", {userName: foundUser.name, BMR: maintenanceCalories, Weight: actualWeight, Unit: weightUnit, gener: foundUser.recipeHistory.length});
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-        // res.render("user", {userName: foundUser.name, BMR: maintenanceCalories, Weight: weight});
-      }
-    }).catch(function(err) {
-      console.log("user error");
-    })
-    
-  } else {
-      res.redirect("/login");
-  }
-});
+//user dashboard get route
+app.use("/user", getUserRoute);
 
 app.get("/form-ai2", function(req, res) {
   if (req.isAuthenticated()) {
@@ -573,20 +483,6 @@ app.post("/result-2", async(req, res) => {
   })
 });
 
-function fetchYouTubeData(apiUrl) {
-  return axios.get(apiUrl)
-    .then((response) => {
-      if (response.status === 200) {
-        return response.data; // Return the JSON data
-      } else {
-        throw new Error(`Error: ${response.status} - ${response.statusText}`);
-      }
-    })
-    .catch((error) => {
-      throw error;
-    });
-}
-
 app.get("/result/:recName", async function(req, res) {
   if (req.isAuthenticated()) {
     const marker = "###SECTION_MARKER###";
@@ -603,6 +499,7 @@ app.get("/result/:recName", async function(req, res) {
     var recName = name.split(':');
     recName = recName[1];
     apiUrl = `https://www.googleapis.com/youtube/v3/search?key=${youtubeKey}&part=snippet&q=${recName}&type=video`
+
     //youtube api call
     youtubeVideos = [];
     try {
